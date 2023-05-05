7984ebf727796c8409d458e2fb07033ff8f9435f
Hxzxrd <hxzxrd@vk.com>
2023-05-05 03:50:15 +0300

    stylized scroll

--- front/myapp/components/layout/Sidebar/Sidebar.tsx
+++
@@ -1,7 +0,0 @@
-import { FC } from 'react'
-
-const Sidebar: FC = () => {
-	return <div>sidebar maybe</div>
-}
-
-export default Sidebar
--- front/myapp/utils/setTheme.ts
+++ front/myapp/utils/setTheme.ts
@@ -13,11 +13,13 @@ export const setTheme = (ref: RefObject<HTMLDivElement>): void => {
 		ref.current?.style.setProperty(`--extra-background`, `#ffffff`)
 		ref.current?.style.setProperty(`--primary-border`, `#dce1e6`)
 		ref.current?.style.setProperty(`--primary-hover`, `#f5f6f8`)
+		ref.current?.style.setProperty(`--primary-scroll`, `#B5B8B1`)
 	} else {
 		ref.current?.style.setProperty(`--primary-color`, `#e0ffff`)
 		ref.current?.style.setProperty(`--primary-background`, `#141414`)
 		ref.current?.style.setProperty(`--extra-background`, `#222222`)
 		ref.current?.style.setProperty(`--primary-border`, `#424242`)
 		ref.current?.style.setProperty(`--primary-hover`, `#323232`)
+		ref.current?.style.setProperty(`--primary-scroll`, `#222222`)
 	}
 }
