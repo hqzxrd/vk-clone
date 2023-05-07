cbf7947bb1f6bf09fb3421c1de49fd5deefa2c06
Hxzxrd <hxzxrd@vk.com>
2023-05-07 09:34:36 +0300

    rewrite toggle theme

--- front/myapp/components/layout/Layout.tsx
+++ front/myapp/components/layout/Layout.tsx
@@ -3,27 +3,28 @@ import Navigation from './Navigation/Navigation'
 import { useRouter } from 'next/router'
 import { FC, useRef } from 'react'
 
+import { useDarkTheme } from '@/hooks/useDarkTheme'
+
 import IChildren from '@/utils/children.inteface'
 
 import styles from './Layout.module.scss'
 
 function isHideNavbar() {
 	const router = useRouter()
+
 	const path = router.pathname
-	return path === '/register' || path === '/login' || path === '/userinfo' ? (
-		``
-	) : (
-		<Navigation />
-	)
+	return path === '/register' || path === '/login' ? `` : <Navigation />
 }
 
 const Layout: FC<IChildren> = ({ children }) => {
 	const ref = useRef<HTMLDivElement>(null)
 
+	const { theme, toggle } = useDarkTheme(ref)
+
 	return (
 		<div className={styles.layout} ref={ref}>
 			<div className={styles.header_wrapper}>
-				<Header refForSetTheme={ref} />
+				<Header toggleTheme={toggle} />
 			</div>
 			<div className={styles.main_wrapper}>
 				<div className={styles.main}>
---
+++ front/myapp/hooks/useDarkTheme.ts
@@ -0,0 +1,41 @@
+import { RefObject, useEffect, useState } from 'react'
+
+/**
+ * Хук который меняет тему, просто нужно передать в него реф на лейаут и вызывать тоггл при клике на какую либо кнопку
+ * @returns {theme} theme - Текущая тема
+ * @returns {toggle} toggle - Тоггл темы
+ */
+export const useDarkTheme = (ref: RefObject<HTMLDivElement>) => {
+	const [theme, setTheme] = useState<boolean>(false)
+
+	function toggle() {
+		setTheme(!theme)
+	}
+
+	useEffect(() => {
+		console.log(`aboba`)
+
+		if (!ref.current?.style) {
+			return
+		}
+		if (!theme) {
+			ref.current.style.setProperty(`--primary-color`, `#000`)
+			ref.current.style.setProperty(`--primary-background`, `#edeef0`)
+			ref.current.style.setProperty(`--extra-background`, `#ffffff`)
+			ref.current.style.setProperty(`--primary-border`, `#dce1e6`)
+			ref.current.style.setProperty(`--primary-hover`, `#f5f6f8`)
+			ref.current.style.setProperty(`--primary-scroll`, `#B5B8B1`)
+			ref.current.style.setProperty(`--primary-button`, `#447bba`)
+		} else {
+			ref.current.style.setProperty(`--primary-color`, `#e0ffff`)
+			ref.current.style.setProperty(`--primary-background`, `#141414`)
+			ref.current.style.setProperty(`--extra-background`, `#222222`)
+			ref.current.style.setProperty(`--primary-border`, `#424242`)
+			ref.current.style.setProperty(`--primary-hover`, `#323232`)
+			ref.current.style.setProperty(`--primary-scroll`, `#222222`)
+			ref.current.style.setProperty(`--primary-button`, `white`)
+		}
+	}, [theme])
+
+	return { theme, toggle }
+}
