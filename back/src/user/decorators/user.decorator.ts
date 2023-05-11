75e8d50a36156091b5bb6326bdf95f5c22017531
Going <goinghiggvp@gmail.com>
2023-05-11 20:16:00 +0300

    update folder decorator to decorators

---
+++ back/src/user/decorators/user.decorator.ts
@@ -0,0 +1,12 @@
+import { ExecutionContext, createParamDecorator } from "@nestjs/common";
+import { UserEntity } from "../entities/user.entity";
+
+type UserDecorator = Pick<UserEntity, 'id' | 'isAuth' | 'email' |'name' |'surname'>
+
+export const User = createParamDecorator(
+    (data: keyof UserDecorator, ctx: ExecutionContext) => {
+        const request = ctx.switchToHttp().getRequest()
+        const user = request.user
+        return data ? user[data] : user
+    }
+)
