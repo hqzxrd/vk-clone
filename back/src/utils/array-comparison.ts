fc5d29fb23112c05c53e1da09d1595c4f51aa424
Going <goinghiggvp@gmail.com>
2023-05-30 21:34:55 +0300

    add examination

--- back/src/utils/array-comparison.ts
+++ back/src/utils/array-comparison.ts
@@ -1,4 +1,5 @@
 export const arrayComparison = <T>(firstArray: T[], secondArray: T[]): T[] => {
+    if(!secondArray || !secondArray.length) return firstArray
      return firstArray.filter(i => !secondArray.includes(i))
     .concat(secondArray.filter(i => !firstArray.includes(i)))
 }
