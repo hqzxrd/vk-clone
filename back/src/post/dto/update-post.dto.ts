4d3beca67eaab7391a5faafe075aa32764b76132
Going <goinghiggvp@gmail.com>
2023-05-30 21:33:55 +0300

    add default value

--- back/src/post/dto/update-post.dto.ts
+++ back/src/post/dto/update-post.dto.ts
@@ -1,11 +1,10 @@
 import { PartialType } from '@nestjs/mapped-types';
 import { CreatePostDto } from './create-post.dto';
-import { IsArray, IsOptional, IsString, ValidateNested } from 'class-validator';
+import { IsArray, IsOptional, IsString } from 'class-validator';
 
 export class UpdatePostDto extends PartialType(CreatePostDto) {
     @IsOptional()
     @IsArray()
     @IsString({each: true})    
-    photos: string[]
-
+    photos: string[] = []
 }
