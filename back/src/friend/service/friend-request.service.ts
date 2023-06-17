15ac7faa3fa293d126125b67add76205af711da2
Going <goinghiggvp@gmail.com>
2023-06-17 12:37:01 +0300

    add getTypeRequest and fix other method

--- back/src/friend/service/friend-request.service.ts
+++ back/src/friend/service/friend-request.service.ts
@@ -1,11 +1,12 @@
 import { BadRequestException, Inject, Injectable, forwardRef } from "@nestjs/common";
 import { InjectRepository } from "@nestjs/typeorm";
 import { FriendRequestEntity } from "../entities/friend-request.entity";
-import { FindOptionsSelect, Repository } from "typeorm";
+import { Repository } from "typeorm";
 import { FriendRequestType } from "../friend-request.enum";
 import { UserEntity } from "src/user/entities/user.entity";
 import { REQUEST_NOT_FOUND } from "../constants/friend.error.constants";
 import { UserService } from "src/user/service/user.service";
+import { Relationship } from "src/user/relationship.enum";
 
 @Injectable()
 export class FriendRequestService {
@@ -16,7 +17,13 @@ export class FriendRequestService {
     ){}
 
     async createRequest(fromUserId: number, toUserId: number) {
+        if(fromUserId === toUserId) throw new BadRequestException()
         const dto = {fromUser: {id: fromUserId}, toUser: {id: toUserId}}
+        const reverseRequestDto = {fromUser: {id: toUserId}, toUser: {id: fromUserId}}
+        
+        const reverseRequest = await this.friendRequestRepository.findOneBy(reverseRequestDto)
+        if(reverseRequest) throw new BadRequestException('Пользователь уже отправил вам заявку')
+
         const oldFriendRequest = await this.friendRequestRepository.findOneBy(dto)
         if(oldFriendRequest) return oldFriendRequest
 
@@ -42,21 +49,31 @@ export class FriendRequestService {
         if(type === FriendRequestType.INCOMING) {
             const friendRequest = await this.friendRequestRepository.findAndCount({
                 ...paginationOptions,
-                relations: {toUser: true},
-                where: {fromUser: {id}},
-                select: { toUser: this.userService.returnBaseKeyUser }
-            })
-            friendRequestAndCount = [friendRequest[0].map(request => request.toUser), friendRequest[1]]
-        }else {
-            const friendRequest = await this.friendRequestRepository.findAndCount({
-                ...paginationOptions,
                 relations: {fromUser: true},
                 where: {toUser: {id}},
                 select: { fromUser: this.userService.returnBaseKeyUser }
             })
             friendRequestAndCount = [friendRequest[0].map(request => request.fromUser), friendRequest[1]]
+        }else {
+            const friendRequest = await this.friendRequestRepository.findAndCount({
+                ...paginationOptions,
+                relations: {toUser: true},
+                where: {fromUser: {id}},
+                select: { toUser: this.userService.returnBaseKeyUser }
+            })
+            friendRequestAndCount = [friendRequest[0].map(request => request.toUser), friendRequest[1]]
         }
 
         return friendRequestAndCount
+    }
+
+    async getTypeRequest(profileUserId: number, userId: number) {
+        const incomingRequest = await this.friendRequestRepository.findOneBy({toUser: {id: userId}, fromUser: {id: profileUserId}})
+        const outgoingRequest = await this.friendRequestRepository.findOneBy({toUser: {id: profileUserId}, fromUser: {id: userId}})
+
+        if(incomingRequest) return Relationship.INCOMING
+        else if(outgoingRequest) return Relationship.OUTGOING
+
+        return Relationship.NONE
     }
 }
