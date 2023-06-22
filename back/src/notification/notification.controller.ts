d9f0b3167b338c81e4c833b44494b6a28cd71d0e
Going <goinghiggvp@gmail.com>
2023-06-23 01:26:29 +0300

    controller notification

--- back/src/notification/notification.controller.ts
+++ back/src/notification/notification.controller.ts
@@ -1,35 +1,29 @@
-import { Controller, Get, Ip, Param, ParseIntPipe, Query, Req, Res, Sse } from '@nestjs/common';
+import { Controller, Get, Ip, Param, ParseIntPipe, Query, Req, Res, Sse, UsePipes, ValidationPipe } from '@nestjs/common';
 import { NotificationService } from './service/notification.service';
 import { AccessJwtGuard } from 'src/auth/decorators/access-jwt.decorator';
 import { User } from 'src/user/decorators/user.decorator';
-import { FastifyReply } from 'fastify';
+import { PaginationQueryDto } from 'src/utils/pagination.query.dto';
 
 @Controller('notification')
 export class NotificationController {
   constructor(private readonly notificationService: NotificationService) {}
 
-  // @AccessJwtGuard()
+  @AccessJwtGuard()
   @Sse('sse')
   handleConnection(
-    @Query('id') id: string
+    @User('id') userId: number
   ){
-    const userId = +id
     return this.notificationService.handleConnection(userId)
   }
 
-  // @Get(':id')
-  // get(
-  //   @Param('id', ParseIntPipe) id: number 
-  // ) {
-  //   // return this.notificationService.send(id, {message: 'ПОшел нахуй'})
-  // } 
+  @AccessJwtGuard()
+  @UsePipes(new ValidationPipe({transform: true}))
+  @Get()
+  getNotification( 
+    @Query() {count, page}: PaginationQueryDto,
+    @User('id') userId: number
 
-  // @AccessJwtGuard()
-  
-  // @Get()
-  // getNotification(
-  //   @User('id') userId: number
-  // ) {
-  //   return this.notificationService.getNotification(userId)
-  // }
+  ) {
+    return this.notificationService.getAll(userId, page, count)
+  }
 }
