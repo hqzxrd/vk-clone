82b9de5f54c0202f9aa6c5e4a5304181f20dde71
Going <goinghiggvp@gmail.com>
2023-06-25 19:39:42 +0300

    add types in enum

--- back/src/notification/enums/notification.type.enum.ts
+++ back/src/notification/enums/notification.type.enum.ts
@@ -1,7 +1,8 @@
 export enum NotificationType {
     MESSAGE = 'message',
     COMMENT = 'comment',
-    LIKE = 'like',
+    LIKE_POST = 'like_post',
+    LIKE_COMMENT = 'like_comment',
     FRIEND_REQUEST = 'friend_request',
     ACCESS_REQUEST = 'access_request'
 }
