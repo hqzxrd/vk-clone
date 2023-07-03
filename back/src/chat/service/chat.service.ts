6e341a3ba51103dd21da83f26b02c3a08971e58b
Going <goinghiggvp@gmail.com>
2023-07-03 17:48:08 +0300

    check user in chat method

--- back/src/chat/service/chat.service.ts
+++ back/src/chat/service/chat.service.ts
@@ -1,4 +1,4 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { ChatEntity } from '../entities/chat.entity';
 import { Repository } from 'typeorm';
@@ -21,5 +21,17 @@ export class ChatService {
 
     const newChat = this.chatRepository.create({users: [{id: ids[0]}, {id: ids[1]}]})
     return await this.chatRepository.save(newChat)
+  }
+
+  async checkUser(chatId: number, userId: number) {
+    const chat = await this.chatRepository.findOne({
+      where: {id: chatId},
+      relations: {users: true},
+      select: {users: {id: true}}
+    })
+    if(!chat) throw new NotFoundException()
+
+    if(chat.users.some(user => user.id === userId)) return true
+    return false
   }
 }
