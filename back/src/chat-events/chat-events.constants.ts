e1d447069ab6b17536169d925b71d7a1da7cbb7c
Going <goinghiggvp@gmail.com>
2023-07-03 17:42:34 +0300

    add event for chat

--- back/src/chat-events/chat-events.constants.ts
+++ back/src/chat-events/chat-events.constants.ts
@@ -1,2 +1,9 @@
 export const PRIVATE_CHAT_EVENT = 'private chat event'
 export const RECEIVE_MESSAGE_EVENT = 'receive message event'
+export const GET_MESSAGES_CHAT_EVENT = 'get messages chat event'
+
+// * message methods
+export const UPDATE_MESSAGE_EVENT = 'update message event'
+export const DELETE_MESSAGE_EVENT = 'delete message event'
+export const RECEIVE_UPDATE_MESSAGE_EVENT = 'receive update message event'
+export const RECEIVE_DELETE_MESSAGE_EVENT = 'receive delete message event'
