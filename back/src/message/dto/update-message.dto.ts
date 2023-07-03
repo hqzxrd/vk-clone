57f6b6671612106b175448a32ce485cc73ddea23
Going <goinghiggvp@gmail.com>
2023-07-03 17:46:40 +0300

    fix update message dto

--- back/src/message/dto/update-message.dto.ts
+++ back/src/message/dto/update-message.dto.ts
@@ -1,4 +1,4 @@
-import { PartialType } from '@nestjs/swagger';
 import { CreateMessageDto } from './create-message.dto';
+import { PickType } from '@nestjs/mapped-types';
 
-export class UpdateMessageDto extends PartialType(CreateMessageDto) {}
+export class UpdateMessageDto extends PickType(CreateMessageDto, ['text']) {}
