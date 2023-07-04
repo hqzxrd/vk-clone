96b8ffaf0d6106e79a109f230a1ea8f247b78ddd
Going <goinghiggvp@gmail.com>
2023-07-05 02:47:14 +0300

    update ws-exception filter

--- back/src/chat-events/filters/ws-exception.filter.ts
+++ back/src/chat-events/filters/ws-exception.filter.ts
@@ -4,7 +4,12 @@ import { BaseWsExceptionFilter, WsException } from "@nestjs/websockets";
 @Catch()
 export class WsExceptionFilter extends BaseWsExceptionFilter {
     catch(exception: HttpException, host: ArgumentsHost): void {
-        const properException = new WsException(exception.getResponse());
-        super.catch(properException, host);
+        console.log(exception)
+        if(exception instanceof HttpException) {
+            const properException = new WsException(exception.getResponse());
+            super.catch(properException, host);
+        }else {
+            super.catch(new WsException({status: 500, message: 'Internal server error'}), host)
+        }
     }
 }
