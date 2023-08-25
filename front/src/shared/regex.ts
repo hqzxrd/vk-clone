1110cfa37c99fbeaef1748d3798ba2bf6bcd9c26
hqzxrd <hxzxrd@vk.com>
2023-08-26 00:37:26 +0300

    nickname validation

--- front/src/shared/regex.ts
+++ front/src/shared/regex.ts
@@ -5,6 +5,8 @@ export const YEAR_REGEX = /^(19[2-9]\d|20[0-1]\d|202[0-3])$/
 
 export const NAME_REGEX = /^[a-zA-Zа-яА-Я]+$/g
 
+export const NICKNAME_REGEX = /^[a-zA-ZА-Яа-я][0-9a-zA-ZА-Яа-я]*$/
+
 export const isEpmtyString = (str: string) => {
   return /\S/g.test(str)
 }
